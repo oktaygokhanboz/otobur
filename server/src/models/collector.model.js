03d34a4fdd4a54a7fb577d271a4ed0170c78f34a
oktaygokhanboz <contact@oktaygokhanboz.com>
2025-07-10 22:53:32 +0300

    refactor(backend): add sorting to seed bank model, add sorting collector model

--- server/src/models/collector.model.js
+++ server/src/models/collector.model.js
@@ -2,7 +2,7 @@ import db from "../config/db.config.js";
 
 const collectorModel = {
   getAll: async () => {
-    const res = await db.query(`SELECT * FROM collector`);
+    const res = await db.query(`SELECT * FROM collector ORDER BY name ASC`);
     return res.rows;
   },
 };
--- server/src/models/seed-bank.model.js
+++ server/src/models/seed-bank.model.js
@@ -4,12 +4,15 @@ const seedBankModel = {
   getAll: async () => {
     const res = await db.query(`
       SELECT
+
       accession_number,
       name AS plant_name,
       quantity,
       locker_code
       FROM seed_bank
+
       JOIN plant ON seed_bank.plant_id = plant.id
+      ORDER BY accession_number ASC
     `);
     return res.rows;
   },
